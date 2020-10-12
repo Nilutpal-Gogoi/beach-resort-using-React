441c1fdcec71e4ef0849015dd5709c4aa4c4e515
nilutpal <nilutpal.ee@gmail.com>
2020-10-12 20:41:34 +0530

    Navbar component

--- src/App.js
+++ src/App.js
@@ -4,16 +4,19 @@ import Home from "./pages/Home"
 import Rooms from "./pages/Rooms"
 import SingleRoom from "./pages/SingleRoom"
 import Error from "./pages/Error"
-
-
+import  {Route, Switch} from "react-router-dom";
+import Navbar from "./components/Navbar"
 
 function App() {
   return (
     <React.Fragment>
-      <Home></Home>
-      <Rooms></Rooms>
-      <SingleRoom></SingleRoom>
-      <Error></Error>
+    <Navbar></Navbar>  {/* Since the navbar is present in every page so we put this around Switch. */}
+    <Switch>
+      <Route exact path="/" component = {Home}></Route>
+      <Route exact path="/rooms/" component = {Rooms}></Route>
+      <Route exact path="/rooms/:slug" component = {SingleRoom}></Route>  
+      <Route component={Error}></Route>  
+    </Switch>
     </React.Fragment>
   )
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 // import './index.css';  
+import {BrowserRouter as Router} from "react-router-dom";
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Router>
+      <App />
+  </Router>,
   document.getElementById('root')
 );
 
@@ -15,3 +16,4 @@ ReactDOM.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+ 
